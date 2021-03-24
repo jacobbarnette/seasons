66936672e4a1b9a29d480195babf43224e78ac57
Jacob Barnette <jacobcbarnette@gmail.com>
2021-03-24 16:34:19 -0400

    completed seasons project

---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const Loader = (props) => {
+    return(
+        <div class="ui active dimmer">
+                    <div class="ui text loader">Loading
+                </div>
+                <p></p>
+            </div>
+    )
+}
+export default Loader
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay'
+import Loader from './Loader'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    state = { lat: null, errorMessage: ''}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({ lat: position.coords.latitude}),
+        err => this.setState({errorMessage: err.message})
+    )
+   }
+
+   renderContent() {
+    if(this.state.errorMessage && !this.state.late) {
+        return(
+            <div>Error: {this.state.errorMessage}</div>
+        )
+    }
+    if(!this.state.errorMessage && this.state.lat) {
+        return(
+            <div><SeasonDisplay lat={this.state.lat} /></div>
+        )
+    }
+
+        return (
+            <div><Loader message='Please accept location request' /> </div>
+            )
+    
+   
+}
+   
+    render() {
+       return (
+           <div>{this.renderContent()}</div>
+       )
+}
+}
+ReactDOM.render(<App />, document.getElementById('root'))
